fe58051a60197f34fade9a65a9dee47ef0f8da8c
MadMag77 <artemswork@mail.ru>
2019-03-02 14:18:35 +0800

    Init commit

---
+++ FlickR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickR
+//
+//  Created by Artem Goncharov on 2/3/19.
+//  Copyright © 2019 madmag77. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,17 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '10.0'
+use_frameworks!
+
+target 'FlickR' do
+	pod 'SnapKit'
+	pod 'ReSwift'
+    pod 'RxSwift',    '~> 4.0'
+    pod 'RxCocoa',    '~> 4.0'
+    pod 'RxDataSources', '~> 3.0'
+end
+
+target 'FlickRTests' do
+	pod 'ReSwift'
+    pod 'RxBlocking', '~> 4.0'
+    pod 'RxTest',     '~> 4.0'
+end
